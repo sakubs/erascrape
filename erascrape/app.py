9acc9e94bc06e2927c82c7d8c639523a2cc41ded
Brian Sakurada <sakuradabs@gmail.com>
2020-01-20 20:13:43 +0900

    Got the request for the web page to scrape working and it pulls all the tags I need for the era information into a beautiful soup.

--- erascrape/app.py
+++ erascrape/app.py
@@ -1,2 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
 def run():
-    print("Hi there")
+    response = requests.get('http://houshinji.org/calendar.html')
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    eras = soup.find_all(class_='era')
+    for era in eras:
+        print(era)
