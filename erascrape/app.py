be242d309e78539a98550b37185d2e25e37b7cea
Brian Sakurada <sakuradabs@gmail.com>
2020-01-20 22:00:33 +0900

    Added a database handler module.

--- erascrape/app.py
+++ erascrape/app.py
@@ -1,10 +1,23 @@
 import requests
+
 from bs4 import BeautifulSoup
+from dbhandler import create_connection, create_table
+
 
 def run():
     response = requests.get('http://houshinji.org/calendar.html')
     soup = BeautifulSoup(response.content, 'html.parser')
-
+    
     eras = soup.find_all(class_='era')
     for era in eras:
-        print(era)
+        
+        for child in era.parent.parent.children:
+            try:
+                print(child.text)
+            except AttributeError:
+                print(child)
+
+    conn = create_connection()
+    create_table(conn)
+    
+    
---
+++ erascrape/dbhandler.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+ERASCRAPE_DB = 'resources/erascrapedb.db3'
+
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(ERASCRAPE_DB)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+def create_table(conn):
+    sql = """CREATE TABLE japanese_eras (
+        id integer PRIMARY_KEY, 
+        name text NOT NULL, 
+        start_date text NOT NULL, 
+        end_date text
+    )"""
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+    except sqlite3.Error as e:
+        print(e)
+
