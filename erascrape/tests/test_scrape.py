1cfd52945ff5713321f230019a0eba6881d9a9ad
Brian Sakurada <sakuradabs@gmail.com>
2020-01-21 19:22:04 +0900

    Wrote 2 tests for make_soup

--- erascrape/tests/test_scrape.py
+++ erascrape/tests/test_scrape.py
@@ -1,8 +1,38 @@
-from erascrape.scrape import make_soup
+import unittest
+import sys
+sys.path.append('..')
+
+from scrape import make_soup
 
 
-def test_make_soup():
-    url = 'http://houshinji.org/calendar.html'
+class TestScrape(unittest.TestCase):
+    """
+    Test suite for testing functions in the scrape module.
+    """
+
+    def test_make_soup_good_url(self):
+        """
+        Want to make sure that the url can be parsed properly, and also that it 
+        handles errors gracefully.
+
+        Give it a good url.
+        """
+        url = 'http://houshinji.org/calendar.html'
+        soup = make_soup(url)
     
-    soup = BeautifulSoup(response.content, 'html.parser')
+    def test_make_soup_bad_url(self):
+        """
+        Want to make sure that the url can be parsed properly, and also that it 
+        handles errors gracefully.
+
+        Give it a bad url.
+        """
+        # bad url
+        url = 'http://housinji.org/calendar.html'
+        soup = make_soup(url)
+
     
+
+
+if __name__ == "__main__":
+    unittest.main()
